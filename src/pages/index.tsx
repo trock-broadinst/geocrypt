03ba75e108903c4d8353ea29972775955fb0e00f
trock-broadinst <123021647+trock-broadinst@users.noreply.github.com>
2023-05-12 10:51:57 -0400

    break off flavtext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import React from "react";
-import { FileUploader } from "react-drag-drop-files";
-import PasswordValidator from "password-validator";
-import Link from "next/link";
 import { vfPart1, vfPart2 } from "@/utils/vaultAssembly_new";
 import {
   BlobReader,
@@ -19,6 +16,7 @@ import "core-js";
 import { showSaveFilePicker } from "native-file-system-adapter";
 import HandleUpload from "@/utils/uploader";
 import HandlePassword from "@/utils/password";
+import FlavorText from "@/utils/flavortext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -163,58 +161,8 @@ export default function Home() {
         <h2 className={styles.description}>
           <div>Warning: it is not advised to use firefox for this website</div>
         </h2>
-
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>
-              Secure <span>⤵</span>
-            </h2>
-            <p>
-              Your files will be compressed and encrypted with AES-CTR, an
-              industry-standard security algorithm
-            </p>
-          </div>
-          <div className={styles.card}>
-            <h2>
-              Simple <span>⤵</span>
-            </h2>
-            <p>
-              The emitted HTML file can be decrypted by anyone with a modern
-              browser, even offline(once necessary libraries have loaded). Be
-              sure to share the password through a secure channel.
-            </p>
-          </div>
-          <div className={styles.card}>
-            <h2>
-              Is this legal? <span>⤵</span>
-            </h2>
-            <p>
-              Some nations have laws against encryption, such as Australia. We
-              don&apos;t care about your location, so use at your own risk. We
-              are not responsible for data loss, illegal use, or any other
-              damages.
-            </p>
-          </div>
-          <div className={styles.card}>
-            <h2>
-              How can I support this? <span>⤵</span>
-            </h2>
-            <Link
-              className={styles.link}
-              href="https://ko-fi.com/edisys/?hidefeed=true&widget=true&embed=true&preview=true"
-            >
-              With Ko-fi
-            </Link>
-            <p>supporting will ensure that</p>
-            <ul>
-              <li>GeoCrypt stays online</li>
-              <li>GeoCrypt gets a new WASM port that allows larger files</li>
-            </ul>
-          </div>
-          <br />
-          copyright 2023
-          <Image src="/edisys.png" alt="edisys logo" width="200" height="100" />
-        </div>
+        <br />
+        <FlavorText />
       </main>
     </>
   );
---
+++ src/utils/flavortext.tsx
@@ -0,0 +1,60 @@
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const FlavorText = () => {
+  return (
+    <div className={styles.grid}>
+      <div className={styles.card}>
+        <h2>
+          Secure <span>⤵</span>
+        </h2>
+        <p>
+          Your files will be compressed and encrypted with AES-CTR, an
+          industry-standard security algorithm
+        </p>
+      </div>
+      <div className={styles.card}>
+        <h2>
+          Simple <span>⤵</span>
+        </h2>
+        <p>
+          The emitted HTML file can be decrypted by anyone with a modern
+          browser, even offline(once necessary libraries have loaded). Be sure
+          to share the password through a secure channel.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <h2>
+          Is this legal? <span>⤵</span>
+        </h2>
+        <p>
+          Some nations have laws against encryption, such as Australia. We
+          don&apos;t care about your location, so use at your own risk. We are
+          not responsible for data loss, illegal use, or any other damages.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <h2>
+          How can I support this? <span>⤵</span>
+        </h2>
+        <Link
+          className={styles.link}
+          href="https://ko-fi.com/edisys/?hidefeed=true&widget=true&embed=true&preview=true"
+        >
+          With Ko-fi
+        </Link>
+        <p>supporting will ensure that</p>
+        <ul>
+          <li>GeoCrypt stays online</li>
+          <li>GeoCrypt gets a new WASM port that allows larger files</li>
+        </ul>
+      </div>
+      <br />
+      copyright 2023
+      <Image src="/edisys.png" alt="edisys logo" width="200" height="100" />
+    </div>
+  );
+};
+
+export default FlavorText;
